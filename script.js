704a6ee6f7c21f62022b6cb2eb240b1319e75486
Maria Migliaris <mariaeduardamigliaris2@gmail.com>
2023-11-15 13:42:58 -0300

    sem animacao

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 new window.VLibras.Widget();
 
-
-document.querySelectorAll('.smooth-scroll').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-
-
-
-AOS.init();
