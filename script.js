d3f86883b26e6d6b9b85f4f90d60bb8c48455f88
Maria Migliaris <mariaeduardamigliaris2@gmail.com>
2023-11-16 20:41:59 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 new window.VLibras.Widget();
 
+
+function scrollToTitulo(event) {
+    event.preventDefault();
+    console.log("Clicou no botão");
+
+    var titulo = document.getElementById('sobre');
+    titulo.scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
+
+document.getElementById('botao-inicial').addEventListener('click', function(event) {
+    scrollToTitulo(event);
+});
+
+
